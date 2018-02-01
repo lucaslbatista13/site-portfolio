2f25a9e96515e8df00b8e6aae5a77c8828abe6d2
lucaslbatista13 <36003184+lucaslbatista13@users.noreply.github.com>
2018-02-01 12:48:39 -0200

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,19 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function aumentaImagem () {
+	
+}
